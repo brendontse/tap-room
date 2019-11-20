cf21ec434490789f49aec5b809463cbf6e161788
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-11-20 00:11:02 -0800

    add initial components

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+const globalStyles = {
+
+};
+
+
+function App(){
+  return (
+    <div style={globalStyles}>
+
+    </div>
+  );
+}
+
+export default App; 
