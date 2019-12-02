99e6b5b742cfe43054dc8b75583f2e69c39539ad
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-12-02 07:11:40 -0800

    add Component Diagram Link to README

---
+++ src/components/Error404.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Error404() {
+  return (
+    <div style={{ textAlign: 'center', padding: '404px' }}>
+      <h1>Error! This page doesn't exist!</h1>
+      <h3>Return <Link to="/">home</Link>.</h3>
+    </div>
+  );
+}
+
+export default Error404;
